75c75365a89bfb3a402275a5531a8905f6cf0771
Karim Hodzic <Karim.Hodzic@gmail.com>
2023-09-10 17:03:37 -0500

    Create 2-1.php

---
+++ Unit2/2-1.php
@@ -0,0 +1,28 @@
+<?php
+
+    $yourName = "Karim Hodzic"
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>2-1: PHP Basics</title>
+</head>
+<body>
+
+    <?php 
+    
+        echo "<h1>2-1: PHP Basics</h1>"
+    
+    ?>
+
+    <h2><?php echo $yourName ?></h2>
+    
+
+
+</body>
+</html>
